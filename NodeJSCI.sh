449e2499bceb745643e8f2ad72655e183f28c653
Yue-plus <Yue_plus@foxmail.com>
2020-10-10 19:40:46 +0800

    update NodeJSCI.sh

--- NodeJSCI.sh
+++ NodeJSCI.sh
@@ -19,7 +19,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-GITHUBURL=https://yue-plus:${GITHUB_TOKEN}@github.com/Yue-plus/NetNote.git
+# GITHUBURL=https://yue-plus:${GITHUB_TOKEN}@github.com/Yue-plus/NetNote.git
+GITHUBURL=git@github.com:Yue-plus/NetNote.git
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
