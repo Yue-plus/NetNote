015ed4e913a071172087a2a2ba38460bea374b0e
Yue-plus <Yue_plus@foxmail.com>
2020-10-10 19:00:39 +0800

    add NodeJSCI.sh

---
+++ NodeJSCI.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'note.yueplus.ink' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git config --global user.name "yue-plus"
+git config --global user.email Yue_plus@foxmail.com
+
+GITHUBURL=https://yue-plus:${ACCESS_TOKEN}@github.com/Yue-plus/NetNote.git
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f $GITHUBURL master:gh-pages
+
+cd -
